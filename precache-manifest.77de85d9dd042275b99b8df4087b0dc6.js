eac7c0d29cb944d3c09f61ca76ef55ab6fe01173
dmytsv <dmitrytsuprunov@Dmitrys-Air.home>
2018-11-26 17:19:14 -0500

    Updates

---
+++ precache-manifest.77de85d9dd042275b99b8df4087b0dc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20f6fe66341f41873373",
+    "url": "/react-pic-search-app/static/js/runtime~main.20f6fe66.js"
+  },
+  {
+    "revision": "948c306387bb6b3ed7fc",
+    "url": "/react-pic-search-app/static/js/main.948c3063.chunk.js"
+  },
+  {
+    "revision": "4090089464dd677b853c",
+    "url": "/react-pic-search-app/static/js/1.40900894.chunk.js"
+  },
+  {
+    "revision": "948c306387bb6b3ed7fc",
+    "url": "/react-pic-search-app/static/css/main.ab1fba66.chunk.css"
+  },
+  {
+    "revision": "b0e98ae9817f736e89cec97a6055b064",
+    "url": "/react-pic-search-app/index.html"
+  }
+];
